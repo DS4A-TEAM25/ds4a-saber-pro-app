92bb13aa01237d5ae11a253fe14c38558ae77c79
rafaeldeaguas <67207822+rafaeldeaguas@users.noreply.github.com>
2020-08-01 12:58:01 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -13,14 +13,36 @@ import joblib
 #from sklearn.model_selection import train_test_split
 import pickle
 
+
 ModelQRPrueba = joblib.load('ModelQR.pkl')
 QR1=np.round(ModelQRPrueba.predict([[59, 59, 59, 22, 6, 0,1,1,1,1,0,1,0,0,0,0,0,0]]),0)
 QR1
 X1=int(QR1[0])
 X1
 
+form = dbc.Form(
+    [
+        dbc.FormGroup(
+            [
+                dbc.Label("ScoreEnglishHighSchool", className="mr-2"),
+                dbc.Input(type="text", placeholder="ScoreEnglishHighSchool"),
+            ],
+            className="mr-3",
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label("State Where you Live", className="mr-2"),
+                dbc.Input(type="text", placeholder="State Where you Live"),
+            ],
+            className="mr-3",
+        ),
+        dbc.Button("Wish me luck!", color="primary"),
+    ],
+    inline=True,
+)
 
 layout = html.Div([
-    html.H1(X1)
+    html.H1(X1),
+    form
 ])
 
