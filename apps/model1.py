2df4e2fdd55844ed20a843b37b450b8cf5949c30
rafaeldeaguas <67207822+rafaeldeaguas@users.noreply.github.com>
2020-08-01 17:17:53 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -29,7 +29,6 @@ posenglish_form = dbc.Form(
                 dbc.Label("posenglish", className="mr-2"),
                 dbc.Input(type="text", placeholder="posenglish"),
             ],
-            justify="around",
             className="mr-3",
         )
 
@@ -46,7 +45,6 @@ program_form = dbc.Form(
             dbc.Label("Program", className="mr-2"),
             dbc.Input(type="text", placeholder="Program"),
         ],
-        justify="around",
         className="mr-3",
     )
     ],
@@ -61,7 +59,6 @@ dbc.FormGroup(
         dbc.Label("ScoreMathHighSchool", className="mr-2"),
         dbc.Input(type="text", placeholder="ScoreMathHighSchool"),
     ],
-    justify="around",
     className="mr-3",
 )
     ],
