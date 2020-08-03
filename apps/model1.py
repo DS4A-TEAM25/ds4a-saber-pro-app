0361c048ce4dfb0d26eb7c6a3f5e428ddbd0ce11
rafaeldeaguas <67207822+rafaeldeaguas@users.noreply.github.com>
2020-08-03 14:55:08 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -556,11 +556,32 @@ esquema = dbc.Row(
 
 
 
+button = html.Div(
+    [
+        dbc.Button("Click me", id="example-button", className="mr-2"),
+        html.Span(id="example-output", style={"vertical-align": "middle"}),
+    ]
+)
+
 
 layout = html.Div([
     html.H1("I will present my exam soon and i want to check how is going to be my result"),
     html.H3(id="mout_01", style={"vertical-align": "middle"}),
     esquema,
+    button
 ])
 
 
+
+
+
+
+
+@app.callback(
+    Output("example-output", "children"), [Input("example-button", "n_clicks")]
+)
+def on_button_click(n):
+    if n is None:
+        return "Not clicked."
+    else:
+        return f"Clicked {n} times."
