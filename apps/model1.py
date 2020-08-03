e5789266adc9cab11ae83f531ae99ec87e111818
rafaeldeaguas <67207822+rafaeldeaguas@users.noreply.github.com>
2020-08-03 13:15:38 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -556,7 +556,7 @@ esquema = dbc.Row(
 
 layout = html.Div([
     html.H1("I will present my exam soon and i want to check how is going to be my result"),
-    html.H3("resultado del modelo", id="mout_01"),
+    html.H3(id="mout_01", style={"vertical-align": "middle"}),
     esquema,   
 ])
    
