3121ad0cad0eff04f5095d587dc7d2437b3b002a
mparoca <36743996+mparoca@users.noreply.github.com>
2020-08-04 07:28:01 -0500

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 from app import server
 from app import app
 # import all pages in the app
-from apps import home, model2, about_us, model1
+from apps import home, model2, about_us, model1, by_location
 
 # building the navigation bar
 dropdown = dbc.DropdownMenu(
@@ -16,8 +16,8 @@ dropdown = dbc.DropdownMenu(
             html.Span([html.I(className="fas fa-home ml-0"), " Home"]), href="/home"),
         dbc.DropdownMenuItem(divider=True),
         dbc.DropdownMenuItem("Describe", header=True),
-        #dbc.DropdownMenuItem(
-         #   html.Span([html.I(className="fas fa-map-marked-alt ml-0"), " Explore by Location"]), href="/by_location"),
+        dbc.DropdownMenuItem(
+            html.Span([html.I(className="fas fa-map-marked-alt ml-0"), " Explore by Location"]), href="/by_location"),
         dbc.DropdownMenuItem(
             html.Span([html.I(className="fas fa-university ml-0"), " Explore by University"]), href="#"),
         dbc.DropdownMenuItem(divider=True),
@@ -101,8 +101,8 @@ def display_page(pathname):
         return about_us.layout
     elif pathname == '/model1':
         return model1.layout
-    #elif pathname == '/by_location':
-     #   return by_location.layout
+    elif pathname == '/by_location':
+        return by_location.layout
     #elif pathname == '/model2':
      #   return model2.layout 
     #elif pathname == '/by_university':
